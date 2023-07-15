f49af87f8ed1fbdedcaaaa59322e16a9764dd6ef
the6p4c <me@doggirl.gay>
2023-07-15 23:39:55 +1000

    toast name when new region selected

--- src/RegionSelector.tsx
+++ src/RegionSelector.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Twemoji from "react-twemoji";
 import { Region } from "./data/data";
 import styles from "./RegionSelector.module.css";
@@ -11,22 +12,37 @@ export interface RegionSelectorProps {
 }
 
 export default function RegionSelector(props: RegionSelectorProps) {
+  const [name, setName] = React.useState("");
+  // Fade out class initially unapplied so that page load/refresh doesn't show region name
+  const [fadeOut, setFadeOut] = React.useState(false);
+
   const flags = props.regions.length === 1 ? null : props.regions.map(({id, name, flag}) => {  
     const isSelected = props.id === id;
+
+    const onClick = () => {
+      setName(name);
+
+      // Remove fade out class and re-add to restart animation
+      setFadeOut(false)
+      setTimeout(() => setFadeOut(true), 10);
+
+      props.onChange(id);
+    };
 
     return <span
       key={id}
       className={`${styles.flag} ${isSelected ? "" : styles.unselected}`}
       title={isSelected ? `Current region is ${name}` : `Change region to ${name}`}
-      onClick={() => props.onChange(id)}
+      onClick={onClick}
     >
       {flag}{" "}
     </span>;
   });
 
   return <div className={props.className}>
-    <Twemoji noWrapper options={{ className: styles.emoji }}>
-      <span>💜 {flags} 🐶</span>
+    <div className={`${styles.name} ${fadeOut ? styles.fadeOut : ""}`}>{name}</div>
+    <Twemoji options={{ className: styles.emoji }}>
+      💜 {flags} 🐶
     </Twemoji>
   </div>;
 }
